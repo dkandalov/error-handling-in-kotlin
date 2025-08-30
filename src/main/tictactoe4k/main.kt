252d318735537e66026abe31431d55d68cb03e61
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2025-08-30 14:53:30 +0100

    switch to Jetty11 server implementation #ai-message

--- src/main/tictactoe4k/main.kt
+++ src/main/tictactoe4k/main.kt
@@ -1,9 +1,9 @@
 package tictactoe4k
 
-import org.http4k.server.ApacheServer
+import org.http4k.server.Jetty11
 import org.http4k.server.asServer
 
 fun main() {
-    WebApp(TicTacToeApp()).asServer(ApacheServer(port = 8080)).start()
+    WebApp(TicTacToeApp()).asServer(Jetty11(port = 8080)).start()
     println("Started web server on http://localhost:8080")
 }
