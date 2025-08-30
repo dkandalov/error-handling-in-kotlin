1ab9042f3436952f97acf337821bb5ec21cf99bb
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2025-08-30 16:58:42 +0100

    Move InMemoryGameStore to a separate file for better modularity

--- src/main/tictactoe4k/game/GameStore.kt
+++ src/main/tictactoe4k/game/GameStore.kt
@@ -1,8 +1,5 @@
 package tictactoe4k.game
 
-import java.util.*
-import java.util.concurrent.ConcurrentHashMap
-
 interface GameStore {
     fun findBy(id: GameId): Game
     fun makeMove(id: GameId, x: Int, y: Int)
@@ -17,26 +14,3 @@ value class GameId(val value: String) {
 
 data class GameNotFound(val id: GameId) : GameException("Game not found: $id")
 
-class InMemoryGameStore(
-    private val gamesById: MutableMap<GameId, Game> = ConcurrentHashMap(),
-    private val generateId: () -> GameId = { GameId(UUID.randomUUID().toString()) },
-) : GameStore {
-    override fun findBy(id: GameId): Game =
-        gamesById[id] ?: throw GameNotFound(id)
-
-    override fun makeMove(id: GameId, x: Int, y: Int) {
-        val updatedGame = findBy(id).makeMove(x, y)
-        update(id, updatedGame)
-    }
-
-    override fun update(id: GameId, game: Game) {
-        if (id !in gamesById.keys) throw GameNotFound(id)
-        gamesById[id] = game
-    }
-
-    override fun add(game: Game): GameId {
-        val id = generateId()
-        gamesById[id] = game
-        return id
-    }
-}
---
+++ src/main/tictactoe4k/game/InMemoryGameStore.kt
@@ -0,0 +1,28 @@
+package tictactoe4k.game
+
+import java.util.*
+import java.util.concurrent.ConcurrentHashMap
+
+class InMemoryGameStore(
+    private val gamesById: MutableMap<GameId, Game> = ConcurrentHashMap(),
+    private val generateId: () -> GameId = { GameId(UUID.randomUUID().toString()) },
+) : GameStore {
+    override fun findBy(id: GameId): Game =
+        gamesById[id] ?: throw GameNotFound(id)
+
+    override fun makeMove(id: GameId, x: Int, y: Int) {
+        val updatedGame = findBy(id).makeMove(x, y)
+        update(id, updatedGame)
+    }
+
+    override fun update(id: GameId, game: Game) {
+        if (id !in gamesById.keys) throw GameNotFound(id)
+        gamesById[id] = game
+    }
+
+    override fun add(game: Game): GameId {
+        val id = generateId()
+        gamesById[id] = game
+        return id
+    }
+}
