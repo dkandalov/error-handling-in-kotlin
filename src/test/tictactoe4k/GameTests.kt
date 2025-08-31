a86c149b857c3c1c4bbb779c697659aabb9df283
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2025-08-31 13:28:26 +0100

    ...

--- src/test/tictactoe4k/GameTests.kt
+++ src/test/tictactoe4k/GameTests.kt
@@ -6,37 +6,39 @@ import strikt.assertions.isEqualTo
 import strikt.assertions.isFalse
 import strikt.assertions.isTrue
 import tictactoe4k.game.*
+import tictactoe4k.game.Player.O
+import tictactoe4k.game.Player.X
 import kotlin.test.assertFailsWith
 
 class GameTests {
     @Test fun `players take turns on each move`() {
         val updatedGame =
-            Game().makeMove(Move(0, 1, Player.X))
-                .makeMove(Move(2, 0, Player.O))
-                .makeMove(Move(2, 1, Player.X))
+            Game().makeMove(Move(0, 1, X))
+                .makeMove(Move(2, 0, O))
+                .makeMove(Move(2, 1, X))
 
         expectThat(updatedGame.moves).isEqualTo(
             listOf(
-                Move(0, 1, Player.X),
-                Move(2, 0, Player.O),
-                Move(2, 1, Player.X)
+                Move(0, 1, X),
+                Move(2, 0, O),
+                Move(2, 1, X)
             )
         )
         expectThat(updatedGame.isOver).isFalse()
-        expectThat(updatedGame.nextPlayer).isEqualTo(Player.O)
+        expectThat(updatedGame.nextPlayer).isEqualTo(O)
     }
 
     @Test fun `player X wins`() {
         val game = Game().makeMoves(playerXWinningMoves)
         expectThat(game.isOver).isTrue()
-        expectThat(game.winner).isEqualTo(Player.X)
+        expectThat(game.winner).isEqualTo(X)
         expectThat(game.nextPlayer).isEqualTo(null)
     }
 
     @Test fun `player O wins`() {
         val game = Game().makeMoves(playerOWinningMoves)
         expectThat(game.isOver).isTrue()
-        expectThat(game.winner).isEqualTo(Player.O)
+        expectThat(game.winner).isEqualTo(O)
         expectThat(game.nextPlayer).isEqualTo(null)
     }
 
@@ -50,27 +52,27 @@ class GameTests {
     @Suppress("RETURN_VALUE_NOT_USED")
     @Test fun `can't make the same move twice`() {
         val game = Game()
-            .makeMove(Move(0, 0, Player.X))
-            .makeMove(Move(1, 1, Player.O))
+            .makeMove(Move(0, 0, X))
+            .makeMove(Move(1, 1, O))
 
-        assertFailsWith<DuplicateMove> { game.makeMove(Move(0, 0, Player.X)) }
+        assertFailsWith<DuplicateMove> { game.makeMove(Move(0, 0, X)) }
     }
 
     @Suppress("RETURN_VALUE_NOT_USED")
     @Test fun `can't make moves outside of the board`() {
-        assertFailsWith<OutOfRangeMove> { Game().makeMove(Move(-1, 0, Player.X)) }
-        assertFailsWith<OutOfRangeMove> { Game().makeMove(Move(0, 3, Player.X)) }
+        assertFailsWith<OutOfRangeMove> { Game().makeMove(Move(-1, 0, X)) }
+        assertFailsWith<OutOfRangeMove> { Game().makeMove(Move(0, 3, X)) }
     }
 
     @Suppress("RETURN_VALUE_NOT_USED")
     @Test fun `can't make moves when the game is over`() {
-        assertFailsWith<MoveAfterGameOver> { Game().makeMoves(playerXWinningMoves).makeMove(Move(2, 2, Player.X)) }
+        assertFailsWith<MoveAfterGameOver> { Game().makeMoves(playerXWinningMoves).makeMove(Move(2, 2, X)) }
     }
 
     @Suppress("RETURN_VALUE_NOT_USED")
     @Test fun `can't make moves with wrong player`() {
-        assertFailsWith<WrongPlayerMove> { Game().makeMove(Move(0, 0, Player.O)) }
-        assertFailsWith<WrongPlayerMove> { Game().makeMove(Move(0, 0, Player.X)).makeMove(Move(0, 1, Player.X)) }
+        assertFailsWith<WrongPlayerMove> { Game().makeMove(Move(0, 0, O)) }
+        assertFailsWith<WrongPlayerMove> { Game().makeMove(Move(0, 0, X)).makeMove(Move(0, 1, X)) }
     }
 
     private fun Game.makeMoves(moves: List<Move>) =
@@ -78,21 +80,21 @@ class GameTests {
 }
 
 val playerXWinningMoves = listOf(
-    Move(0, 0, Player.X), Move(1, 0, Player.O),
-    Move(0, 1, Player.X), Move(1, 1, Player.O),
-    Move(0, 2, Player.X)
+    Move(0, 0, X), Move(1, 0, O),
+    Move(0, 1, X), Move(1, 1, O),
+    Move(0, 2, X)
 )
 
 val playerOWinningMoves = listOf(
-    Move(0, 1, Player.X), Move(0, 0, Player.O),
-    Move(0, 2, Player.X), Move(1, 1, Player.O),
-    Move(1, 0, Player.X), Move(2, 2, Player.O)
+    Move(0, 1, X), Move(0, 0, O),
+    Move(0, 2, X), Move(1, 1, O),
+    Move(1, 0, X), Move(2, 2, O)
 )
 
 val gameEndsInDrawMoves = listOf(
-    Move(1, 1, Player.X), Move(0, 0, Player.O),
-    Move(0, 1, Player.X), Move(0, 2, Player.O),
-    Move(1, 0, Player.X), Move(1, 2, Player.O),
-    Move(2, 0, Player.X), Move(2, 1, Player.O),
-    Move(2, 2, Player.X)
+    Move(1, 1, X), Move(0, 0, O),
+    Move(0, 1, X), Move(0, 2, O),
+    Move(1, 0, X), Move(1, 2, O),
+    Move(2, 0, X), Move(2, 1, O),
+    Move(2, 2, X)
 )
