aed628e8ca791ee6a639e218a88c530f54036f73
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2025-08-31 10:10:52 +0100

    Refactor UserCookieTests to simplify cookie handling logic

--- src/test/tictactoe4k/UserCookieTests.kt
+++ src/test/tictactoe4k/UserCookieTests.kt
@@ -3,6 +3,7 @@ package tictactoe4k
 import org.http4k.core.Method.GET
 import org.http4k.core.Request
 import org.http4k.core.cookie.Cookie
+import org.http4k.core.cookie.cookie
 import org.http4k.core.cookie.cookies
 import org.junit.jupiter.api.Test
 import strikt.api.expectThat
@@ -17,13 +18,15 @@ class UserCookieTests {
         expectThat(response.cookies()).isEqualTo(listOf(Cookie("userid", "1", path = "/", httpOnly = true)))
     }
     @Test fun `does not overwrite existing userid cookie`() {
-        val first = webApp(Request(GET, "/"))
-        val setCookie = first.headers.firstNotNullOf { (name, value) -> if (name.equals("Set-Cookie", ignoreCase = true) && value?.startsWith("userid=") == true) value else null }
-        val value = setCookie.substringAfter("userid=").substringBefore(';')
+        val response = webApp(Request(GET, "/").cookie("userid", "123"))
+        expectThat(response.cookies()).isEqualTo(emptyList())
 
-        val response = webApp(Request(GET, "/").header("Cookie", "userid=$value"))
-        // Should not set a new cookie when one is already present
-        val cookies = response.headers.mapNotNull { (name, v) -> if (name.equals("Set-Cookie", ignoreCase = true) && v?.startsWith("userid=") == true) v else null }
-        expectThat(cookies.size).isEqualTo(0)
+//        val setCookie = first.headers.firstNotNullOf { (name, value) -> if (name.equals("Set-Cookie", ignoreCase = true) && value?.startsWith("userid=") == true) value else null }
+//        val value = setCookie.substringAfter("userid=").substringBefore(';')
+//
+//        val response = webApp(Request(GET, "/").header("Cookie", "userid=$value"))
+//        // Should not set a new cookie when one is already present
+//        val cookies = response.headers.mapNotNull { (name, v) -> if (name.equals("Set-Cookie", ignoreCase = true) && v?.startsWith("userid=") == true) v else null }
+//        expectThat(cookies.size).isEqualTo(0)
     }
 }
