a1024d3e3f8e70d6528bc322f436c7cd96cec920
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2025-09-01 23:24:39 +0100

    ...

--- src/main/resources/public/game.js
+++ src/main/resources/public/game.js
@@ -1,8 +1,8 @@
-function onPageLoad() {
-    const es = new EventSource('/game/events?gameId={{gameId}}');
+function onPageLoad(gameId) {
+    const es = new EventSource(`/game/events?gameId=${gameId}`);
     es.addEventListener('update', function(event) {
-        const gameId = event.data;
-        if (gameId === '{{gameId}}') location.reload();
-        else location.assign(`/game/events?gameId=${gameId}`)
+        const updatedGameId = event.data;
+        if (updatedGameId === gameId) location.reload();
+        else location.assign(`/game/${updatedGameId}`)
     });
 }
