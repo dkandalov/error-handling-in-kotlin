198be7b2329c4127b91ca861ae42e31ce2deeeb4
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2025-09-05 11:03:56 +0100

    ...

--- src/main/tictactoe4k/main.kt
+++ src/main/tictactoe4k/main.kt
@@ -3,10 +3,22 @@ package tictactoe4k
 import org.http4k.routing.poly
 import org.http4k.server.Jetty11
 import org.http4k.server.asServer
+import tictactoe4k.game.H2GameStore
 import tictactoe4k.game.InMemoryGameStore
 
 fun main() {
-    val webApp = WebApp(InMemoryGameStore())
-    poly(webApp.httpHandler, webApp.sseHandler).asServer(Jetty11(port = 8080)).start()
-    println("Started web server on http://localhost:8080")
+    val config = Config()
+    val gameStore =
+        if (config.useInMemoryStore) InMemoryGameStore()
+        else H2GameStore(config.h2JdbcUrl)
+    val webApp = WebApp(gameStore)
+
+    poly(webApp.httpHandler, webApp.sseHandler).asServer(Jetty11(port = config.port)).start()
+    println("Started web server on http://localhost:${config.port}")
 }
+
+data class Config(
+    val port: Int = System.getenv("PORT")?.toInt() ?: 8080,
+    val useInMemoryStore: Boolean = System.getenv("IN_MEMORY_STORE")?.toBoolean() ?: false,
+    val h2JdbcUrl: String = System.getenv("JDBC_DATABASE_URL") ?: "jdbc:h2:file:./data",
+)
