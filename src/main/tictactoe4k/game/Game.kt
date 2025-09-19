747494acc61402e33987eff3e1fe6a06e9e29c15
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2025-09-19 16:27:36 +0200

    ...

--- src/main/tictactoe4k/game/Game.kt
+++ src/main/tictactoe4k/game/Game.kt
@@ -1,5 +1,9 @@
 package tictactoe4k.game
 
+import dev.forkhandles.result4k.Result
+import dev.forkhandles.result4k.asFailure
+import dev.forkhandles.result4k.asSuccess
+import dev.forkhandles.result4k.orThrow
 import tictactoe4k.game.Player.O
 import tictactoe4k.game.Player.X
 
@@ -8,13 +12,16 @@ data class Game(val moves: List<Move> = emptyList()) {
     val isOver = winner != null || moves.size == 9
     val nextPlayer = if (isOver) null else if (moves.lastOrNull()?.player == X) O else X
 
-    fun makeMove(move: Move): Game {
+    fun makeMove(move: Move): Game =
+        makeMove_new(move).orThrow()
+
+    fun makeMove_new(move: Move): Result<Game, WrongPlayerMove> {
         if (isOver) throw MoveAfterGameOver(move)
         if (move.x !in 0..2 || move.y !in 0..2) throw OutOfRangeMove(move)
         if (moves.any { it.x == move.x && it.y == move.y }) throw DuplicateMove(move)
-        if (move.player != nextPlayer) throw WrongPlayerMove(move.player)
+        if (move.player != nextPlayer) return WrongPlayerMove(move.player).asFailure()
 
-        return Game(moves + move)
+        return Game(moves + move).asSuccess()
     }
 
     private fun findWinner(): Player? =
